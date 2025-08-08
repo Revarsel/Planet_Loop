184901bfbf9b2cc4e6165be820ae89f10ecd6140
Revarsel <mastermindofall111@gmail.com>
2025-08-08 20:08:05 +0530

    Minor changes to naming of variables

--- Scripts/earth_2.gd
+++ Scripts/earth_2.gd
@@ -5,18 +5,18 @@ var max_angle = deg_to_rad(90)
 var direction = Vector2(-1, 1)
 var s = false
 var fac = 0.01
-var max = 0.66
-var min = 0.61
+var max_val = 0.66
+var min_val = 0.61
 func _process(delta):
 	rotation = lerp_angle(min_angle, max_angle, elapsed)
 	elapsed+=delta*0.03
 	if s == false:
 	#scale<=Vector2(0.58,0.58) && scale>=Vector2(0.38,0.38):
 		scale+=Vector2(fac*delta,fac*delta)
-		if scale>=Vector2(max,max):
+		if scale>=Vector2(max_val,max_val):
 			s = true
 	if s == true:
 	#scale<=Vector2(0.38,0.38):
 		scale-=Vector2(fac*delta,fac*delta)
-		if scale<=Vector2(min,min):
+		if scale<=Vector2(min_val,min_val):
 			s = false
--- Scripts/mars_2_copy_2_scaled.gd
+++ Scripts/mars_2_copy_2_scaled.gd
@@ -5,18 +5,18 @@ var max_angle = deg_to_rad(90)
 var direction = Vector2(-1, 1)
 var s = false
 var fac = 0.01
-var max = 0.255
-var min = 0.205
+var max_val = 0.255
+var min_val = 0.205
 func _process(delta):
 	rotation = lerp_angle(min_angle, max_angle, elapsed)
 	elapsed+=delta*0.05
 	if s == false:
 	#scale<=Vector2(0.58,0.58) && scale>=Vector2(0.38,0.38):
 		scale+=Vector2(fac*delta,fac*delta)
-		if scale>=Vector2(max,max):
+		if scale>=Vector2(max_val,max_val):
 			s = true
 	if s == true:
 	#scale<=Vector2(0.38,0.38):
 		scale-=Vector2(fac*delta,fac*delta)
-		if scale<=Vector2(min,min):
+		if scale<=Vector2(min_val,min_val):
 			s = false
